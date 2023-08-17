e375a5adfd8a8e9d4a35242220064a36b5c638dc
Lambseb <116831255+Lambseb@users.noreply.github.com>
2023-08-17 18:51:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,39 @@
-var showHeight = window.innerHeight;
-var showWidth = window.innerWidth;
-console.log(showHeight, showWidth);
 
-
-// script show list menu
-
-
-/* crée les const pour chaque bouton !!
-
-btnSalad.addEventListener("click", () => {
-    saladeSlide.style.display = "flex";
-    burgerSlide.style.display = "none";
-    tartineSlide.style.display ="none";
-   if (saladeSlide.style.display === "flex"){
-    saladeSlide.style.transition = "opacity 2s ease";
-    saladeSlide.style.opacity = "0";
-   }
+// activate movie presentation " le cadre"
+const leCadre = document.querySelector('.le_cadre');
+const switchMovie = document.querySelector('.wrapperMain');
+const movie = document.querySelector('.video');
+const mvTitle = document.querySelector('.mv_title')
+const closeVid = document.querySelector('.cut_video');
+const text = document.querySelector('.title_wrapper')
+leCadre.addEventListener('click',() =>{
+    mvTitle.classList.add('mv_title_reveal');
+    closeVid.classList.add('cut_video_reveal');
+    text.style.opacity = '0';
 })
-btnTartine.addEventListener("click", () => {
-    saladeSlide.style.display = "none";
-    burgerSlide.style.display = "none";
-    tartineSlide.style.display ="flex";
-    if (tartineSlide.style.display === "flex"){
-        tartineSlide.style.transition = "opacity 2s ease";
-        tartineSlide.style.opacity ="0";
-    }
-})
-btnBurger.addEventListener("click", () => {
-    saladeSlide.style.display = "none";
-    burgerSlide.style.display = "flex";
-    tartineSlide.style.display = "none";
+closeVid.addEventListener('click', () =>{
+    mvTitle.classList.remove('mv_title_reveal');
+    closeVid.classList.remove('cut_video_reveal');
 })
 
-const btnSlide = document.querySelector(".btn-slide");
-var state = 1;
-const btnSalad = document.querySelector(".btn-salad");
-const btnTartine = document.querySelector(".btn-tartine");
-const btnBurger = document.querySelector(".btn-burger");
-const saladeSlide = document.querySelector(".salade");
-const tartineSlide = document.querySelector(".tartine");
-const burgerSlide = document.querySelector(".burger");
-const platSlide = document.querySelector(".plat");
-btnSlide.addEventListener("click", () => {
-    if (state === 0) {
-        tartineSlide.style.display ="none";
-        saladeSlide.style.display ="flex";
-        burgerSlide.style.display ="none";
-        platSlide.style.display ="none";
-        state = 1;
-    } else if (state === 1) {
-        burgerSlide.style.display ="none"
-        tartineSlide.style.display ="flex";
-        saladeSlide.style.display ="none";
-        platSlide.style.display ="none";
-        state = 2;
-    } else if (state === 2) {
-        saladeSlide.style.display="none";
-        tartineSlide.style.display ="none";
-        burgerSlide.style.display ="flex";
-        platSlide.style.display ="none";
-        state = 3;
-    } else if (state === 3) {
-    saladeSlide.style.display="none";
-    tartineSlide.style.display ="none";
-    burgerSlide.style.display ="none";
-    platSlide.style.display ="flex";
-    state = 0;
-}
-});
-*/
+const ratio = 0.5;
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio
+};
+const objectObs = document.querySelectorAll('.img_pic_menu');
+
+function handleIntersect (entries){
+  //   console.log(entries);
+    entries.forEach( entry => {
+     if (entry.intersectionRatio > ratio) {
+         entry.target.classList.add('pics_visible');
+         observer.unobserve(entry.target);
+     }
+})}
+const observer = new IntersectionObserver(handleIntersect, options);
+objectObs.forEach(imageScrolled =>{
+    observer.observe(imageScrolled)
+})
+
